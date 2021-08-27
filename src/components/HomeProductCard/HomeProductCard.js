12061b993eea110c4f186be4fc5b1267c8f5e2e5
Ben Walpole <benwalpole95@gmail.com>
2021-08-27 21:04:07 +0100

    adding more products

--- src/components/HomeProductCard/HomeProductCard.js
+++ src/components/HomeProductCard/HomeProductCard.js
@@ -1,40 +1,64 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react"
-import { FaArrowCircleRight } from "react-icons/fa";
+import { useState } from "react";
+import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 import styles from "./HomeProductCard.module.scss";
 
-function HomeProductCard({product}) {
-    const {groupId, variants, groupTitle} = product;
-    const [imgIndex, setImgIndex] = useState(0)
-    function showRightImg(e, id) {
-        e.preventDefault();
-        e.stopPropagation();
-        console.log('event target', e.target)
-        console.log('id: ', id)
-        setImgIndex(prev => prev + 1);
-      }
+function HomeProductCard({ product }) {
+  const { groupId, variants, groupTitle } = product;
+  const [imgIndex, setImgIndex] = useState(0);
 
-    return (
-        <li key={groupId} className={styles.product}>
-          <Link href={`/products/${groupId}`}>
-            <a>
-              <div className={styles.imgWrapper}>
-                <Image
-                  width="4032"
-                  height="3024"
-                  src={variants[imgIndex].image}
-                  alt={`Card of ${groupTitle}`}
+  function showRightImg(e, id) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (imgIndex === product.variants.length - 1) {
+      setImgIndex(0);
+    } else {
+      setImgIndex((prev) => prev + 1);
+    }
+  }
+
+  function showLeftImg(e, id) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (imgIndex === 0) {
+      setImgIndex(product.variants.length - 1);
+    } else {
+      setImgIndex((prev) => prev - 1);
+    }
+  }
+
+  return (
+    <li key={groupId} className={styles.product}>
+      <Link href={`/products/${groupId}`}>
+        <a>
+          <div className={styles.imgWrapper}>
+            <Image
+              width="4032"
+              height="3024"
+              src={variants[imgIndex].image}
+              alt={`Card of ${groupTitle}`}
+            />
+            {product.variants.length > 1 && (
+              <>
+                <FaArrowCircleRight
+                  id={groupId}
+                  className={`${styles.arrow} ${styles.right}`}
+                  onClick={(e) => showRightImg(e, groupId)}
                 />
-                <FaArrowCircleRight id={groupId} className={styles.arrow} onClick={(e) => showRightImg(e, groupId)}/>
-              </div>
-              <h3 className={styles.productTitle}>
-                {groupTitle}
-              </h3>
-            </a>
-          </Link>
-        </li>
-      );
+                <FaArrowCircleLeft
+                  id={groupId}
+                  className={`${styles.arrow} ${styles.left}`}
+                  onClick={(e) => showLeftImg(e, groupId)}
+                />
+              </>
+            )}
+          </div>
+          <h3 className={styles.productTitle}>{groupTitle}</h3>
+        </a>
+      </Link>
+    </li>
+  );
 }
 
-export default HomeProductCard
+export default HomeProductCard;
