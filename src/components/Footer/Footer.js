9faa027f98c6cfcacc47674c1734dca555549db2
Ben Walpole <benwalpole95@gmail.com>
2021-08-29 11:47:45 +0100

    updating footer links and styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,61 @@
+import Image from "next/image";
 import styles from "./Footer.module.scss";
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>
-        Contact us at gemmaruttermasks@gmail.<span>com</span> or at{" "}
+      <div className={styles.contact}>
+        <p>
+          Contact us at gemmaruttermasks@gmail.<span>com</span> or at{" "}
+          <a
+            href="https://instagram.com/gemmaruttermasks?igshid=1riu84ysctghb"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.instaLink}
+          >
+            gemmaruttermasks
+          </a>{" "}
+          on Instagram
+        </p>
         <a
           href="https://instagram.com/gemmaruttermasks?igshid=1riu84ysctghb"
           target="_blank"
           rel="noopener noreferrer"
-          className={styles.instaLink}
+          className={styles.instaLogo}
         >
-          gemmaruttermasks
-        </a>{" "}
-        on Instagram.
+          <Image
+            src="/images/instalogo.png"
+            alt="Instagram Logo"
+            width={50}
+            height={50}
+            //layout="responsive"
+          />
+        </a>
+      </div>
+
+      <div className={styles.credits}>
+        <p className={styles.p}>Website made by Ben Walpole</p>
+        <a
+          href="https://github.com/btwalpole/gemmaruttermasks"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.githubLink}
+        >
+          <Image
+            src="/images/GitHub_Mark.png"
+            alt="GitHub Logo"
+            width={32}
+            height={32}
+            layout="intrinsic"
+          />
+        </a>
+      </div>
+      <p className={`${styles.copyrightText} ${styles.p}`}>
+        <p className={`${styles.copyrightLogo} ${styles.p}`}>&#xa9;</p>
+        <p className={styles.p}>
+          Gemma Rutter Masks, {new Date().getFullYear()}
+        </p>
       </p>
-      &copy; Gemma Rutter Masks, {new Date().getFullYear()}
     </footer>
   );
 }
